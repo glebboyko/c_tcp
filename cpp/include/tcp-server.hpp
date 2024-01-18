10b2f33b8fb12f636dc30c14d4345b80437cc492
Gleb Boiko <glebboyko@icloud.com>
2024-01-18 19:28:16 +0300

    Changed server algo

--- cpp/include/tcp-server.hpp
+++ cpp/include/tcp-server.hpp
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <map>
+#include <mutex>
+#include <queue>
+#include <semaphore>
+#include <thread>
+
 #include "tcp-client.hpp"
 
 namespace TCP {
 
 class TcpServer {
  public:
-  TcpServer(int port, logging_foo f_logger,
-            int max_queue_length = 1);
-  TcpServer(int port, int max_queue_length = 1);
+  TcpServer(int port, logging_foo f_logger);
+  TcpServer(int port);
   ~TcpServer();
 
   TcpClient AcceptConnection();
@@ -17,9 +22,24 @@ class TcpServer {
   bool IsListenerOpen() const noexcept;
 
  private:
+  static const int kMaxClientLength = 1024;
+  static const int kLoopMsTimeout = 100;
+
   int listener_;
+  bool is_active_ = true;
+  int port_;
+
+  std::queue<TcpClient> accepted_;
+  std::mutex accept_mutex_;
+  std::counting_semaphore<kMaxClientLength> accepter_semaphore_ =
+      std::counting_semaphore<kMaxClientLength>(0);
+
+  std::map<uint64_t, int> uncomplete_client_;
 
   logging_foo logger_;
+
+  std::thread accept_thread_;
+  void AcceptLoop() noexcept;
 };
 
 }  // namespace TCP
--- cpp/source/tcp-server.cpp
+++ cpp/source/tcp-server.cpp
@@ -13,8 +13,8 @@
 
 namespace TCP {
 
-TcpServer::TcpServer(int port, logging_foo f_logger, int max_queue_length)
-    : logger_(f_logger) {
+TcpServer::TcpServer(int port, logging_foo f_logger)
+    : port_(port), logger_(f_logger) {
   LServer logger(LServer::FConstructor, this, logger_);
 
   logger.Log("Trying to create socket", Debug);
@@ -28,57 +28,176 @@ TcpServer::TcpServer(int port, logging_foo f_logger, int max_queue_length)
                       .sin_port = htons(port),
                       .sin_addr = {htonl(INADDR_ANY)}};
 
-  logger.Log("Trying to bind to " + std::to_string(port), Info);
+  logger.Log("Trying to bind to " + std::to_string(port), Debug);
   if (bind(listener_, (sockaddr*)&addr, sizeof(addr)) < 0) {
     close(listener_);
     throw TcpException(TcpException::Binding, logger_, errno);
   }
-  logger.Log("Bound", Info);
+  logger.Log("Bound", Debug);
 
-  logger.Log("Trying to listen", Info);
-  if (listen(listener_, max_queue_length) < 0) {
+  logger.Log("Trying to listen", Debug);
+  if (listen(listener_, kMaxClientLength) < 0) {
     close(listener_);
     throw TcpException(TcpException::Listening, logger_, errno);
   }
-  logger.Log("Listening", Info);
+  logger.Log("Listening", Debug);
+
+  logger.Log("Creating accepter thread", Debug);
+  accept_thread_ = std::thread(&TcpServer::AcceptLoop, this);
+  logger.Log(
+      "Server on port " + std::to_string(port) + " successfully launcher",
+      Info);
 }
-TcpServer::TcpServer(int port, int max_queue_length)
-    : TcpServer(port, LoggerCap, max_queue_length) {}
+TcpServer::TcpServer(int port) : TcpServer(port, LoggerCap) {}
 
 TcpServer::~TcpServer() {
   LServer logger(LServer::FDestructor, this, logger_);
+
+  is_active_ = false;
   if (listener_ != 0) {
     close(listener_);
-    logger.Log("Stopped listening", Info);
+    logger.Log("Stopped listening", Debug);
+    logger.Log("Joining accepter thread", Debug);
+    accept_thread_.join();
   } else {
-    logger.Log("Listening has already been stopped", Info);
+    logger.Log("Listening has already been stopped and joined", Debug);
   }
+
+  logger.Log("Server destructed", Info);
 }
 
 TcpClient TcpServer::AcceptConnection() {
   LServer logger(LServer::FAccepter, this, logger_);
 
-  logger.Log("Trying to accept connection", Info);
-  int client = accept(listener_, NULL, NULL);
+  logger.Log("Waiting for data is available", Debug);
+  accepter_semaphore_.acquire();
+  logger.Log("Get data availability flag. Locking mutex", Debug);
+  accept_mutex_.lock();
+  logger.Log("Mutex locked", Debug);
 
-  if (client < 0) {
-    if (errno == ECONNABORTED) {
-      throw TcpException(TcpException::ConnectionBreak, logger_);
-    }
-    throw TcpException(TcpException::Acceptance, logger_, errno);
+  if (accepted_.empty()) {
+    logger.Log("Data is not available", Warning);
+    accept_mutex_.unlock();
+    throw TcpException(TcpException::NoData, logger_);
   }
 
-  logger.Log(LogSocket(client) + "Connection accepted", Info);
-  return TcpClient(client, logger_);
+  logger.Log("Extracting client", Debug);
+  TcpClient client = std::move(accepted_.front());
+  accepted_.pop();
+
+  logger.Log("Unlocking mutex", Debug);
+  accept_mutex_.unlock();
+
+  logger.Log("Returning client", Debug);
+  return client;
 }
 
 void TcpServer::CloseListener() noexcept {
   LServer logger(LServer::FCloseListener, this, logger_);
 
+  is_active_ = false;
   close(listener_);
-  logger.Log("Listener closed", Info);
+  logger.Log("Listener closed", Debug);
   listener_ = 0;
+  logger.Log("Waiting for accepter joining", Debug);
+  accept_thread_.join();
+  logger.Log("Listener closed. Accepter joined", Info);
 }
 bool TcpServer::IsListenerOpen() const noexcept { return listener_ != 0; }
+
+void TcpServer::AcceptLoop() noexcept {
+  LServer logger(LServer::FLoopAccepter, this, logger_);
+  logger.Log("Starting accepter loop", Debug);
+
+  int64_t password = 1;
+  while (is_active_) {
+    try {
+      logger.Log("Starting waiting for acceptance", Debug);
+      if (!WaitForData(listener_, kLoopMsTimeout, logger, logger_)
+               .has_value()) {
+        logger.Log("Acceptance timeout", Debug);
+        continue;
+      }
+      logger.Log("Client is waiting for accept. Accepting", Debug);
+      int client = accept(listener_, NULL, NULL);
+
+      if (!is_active_) {
+        logger.Log("Got terminating flag. Closing connection. Terminating",
+                   Debug);
+        close(client);
+        return;
+      }
+      if (client < 0) {
+        logger.Log("Error occurred while accepting connection", Warning);
+        TcpException(TcpException::Acceptance, logger_, errno);
+        continue;
+      }
+      logger.Log("Connection accepted", Debug);
+
+      logger.Log("Waiting for client to send password", Debug);
+      if (!WaitForData(client, kLoopMsTimeout, logger, logger_).has_value()) {
+        logger.Log("Waiting timeout. Sending term signal", Warning);
+        RawSend(client, "0", kULLMaxDigits + 1);
+        continue;
+      }
+      logger.Log("Client config is available. Trying to receive", Debug);
+      auto mode_str = RawRecv(client, kULLMaxDigits + 1);
+      if (mode_str.empty()) {
+        logger.Log("Error occurred while receiving config. Sending term signal",
+                   Warning);
+        TcpException(TcpException::Receiving, logger_, errno);
+        RawSend(client, "0", kULLMaxDigits + 1);
+        close(client);
+        continue;
+      }
+
+      int64_t client_password = std::stoll(mode_str);
+      logger.Log("Got client config", Debug);
+
+      if (client_password == 0) {
+        logger.Log("Client is in init mode. Sending password", Debug);
+        if (RawSend(client, std::to_string(password), kULLMaxDigits + 1) ==
+            kULLMaxDigits + 1) {
+          logger.Log("Password sent successfully", Debug);
+          uncomplete_client_.erase(client_password);
+          uncomplete_client_.insert({client_password, client});
+        } else {
+          logger.Log(
+              "Error occurred while sending password. Closing connection",
+              Warning);
+          close(client);
+        }
+      } else if (uncomplete_client_.contains(client_password)) {
+        logger.Log("Client sent password. Tmp table contains connected peer",
+                   Debug);
+        int client_recv = uncomplete_client_[client_password];
+        uncomplete_client_.erase(client_password);
+        if (RawSend(client, "1", 1) == 1) {
+          logger.Log("Sent run signal. Locking mutex", Debug);
+          accept_mutex_.lock();
+          logger.Log("Mutex locked. Creating TcpClient", Debug);
+          accepted_.emplace(client_recv, client, logger_);
+          accept_mutex_.unlock();
+          logger.Log("Mutex unlocked", Debug);
+        } else {
+          logger.Log(
+              "Error occurred while sending run signal. Closing connections",
+              Warning);
+          close(client);
+          close(client_recv);
+        }
+      } else {
+        logger.Log(
+            "Client sent password. Tmp table does not contain connected peer",
+            Warning);
+        RawSend(client, "0", 1);
+        close(client);
+      }
+    } catch (TcpException& exception) {
+      logger.Log("Caught exception", Debug);
+    }
+    password = (password % LONG_LONG_MAX) + 1;
+  }
+}
 
 }  // namespace TCP
